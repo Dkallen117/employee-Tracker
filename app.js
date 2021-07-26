35612549b9c60daca90135e764b912a928768a2c
Daniel Allen <danielkentonallen@gmail.com>
2021-07-26 18:38:39 -0400

    forgot semicolon

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ viewAllRoles = () => {
 
 viewAllDepartments = () => {
 
-    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id", 
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
     (err, res) => {
     if (err) throw err
     console.table(res);
